7bb8fec1d9e02c4f74aefead3ca5b750c38a3c82
taufiqgiri <taufiqgiri.math@gmail.com>
2020-12-11 13:44:24 +0700

    menambahkan dan mengupdate server: 1. game logic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const randomWords = require('random-words')
 const PORT = process.env.NODE_ENV || 3000
 
 let rooms = []
@@ -17,10 +18,10 @@ io.on('connect', socket => {
     }
     rooms.push(room)
     console.log(rooms)
-    socket.emit('fetchRooms', rooms)
+    io.emit('fetchRooms', rooms)
   })
   socket.on('updatedRoom', _ => {
-    socket.emit('fetchRooms', rooms)
+    io.emit('fetchRooms', rooms)
   })
   socket.on('joinRoom', payload => {
     socket.join(payload.roomname, () => {
@@ -31,11 +32,39 @@ io.on('connect', socket => {
             score: payload.score
           }
           rooms[i].users.push(user)
+          io.emit('fetchRooms', rooms)
           io.sockets.in(rooms[i].roomname).emit('fetchPlayers', rooms[i])
-          console.log(rooms)
         }
       }
     })
+  })
+  socket.on('startGame', (payload) => {
+    io.to(payload.roomname).emit('init', randomWords())
+  })
+  socket.on('newAnswer', payload => {
+    let nextword = ''
+    for (let i = 0; i < rooms.length; i++) {
+      if (rooms[i].roomname === payload.roomname) {
+        rooms[i].counter = 1
+        for (let j = 0; j < rooms[i].users.length; j++) {
+          if (rooms[i].users[j].username === payload.username) {
+            if (payload.answer === payload.word) {
+              rooms[i].users[j].score += 10
+            }
+            io.sockets.in(rooms[i].roomname).emit('fetchPlayers', rooms[i])
+            if (payload.counter < 5) {
+              io.to(payload.roomname).emit('init', randomWords())
+            } else {
+              let highestScore = Math.max.apply(Math, rooms[i].users.map(function(win) { return win.score }))
+              let winner = rooms[i].users.filter(function(element) {
+                return (element.score === highestScore)
+              })
+              io.to(rooms[i].roomname).emit('finalScore', winner)
+            }
+          }
+        }
+      }
+    }
   })
 });
 
